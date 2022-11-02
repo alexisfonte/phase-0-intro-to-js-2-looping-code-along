cdea0fac0f92cd773c4e2f92fb7c239f1aba56d2
Alexis Fonte <alexis.e.fonte@gmail.com>
2022-11-01 22:10:07 -0500

    commit tree 92635e819b81a26f944ec4413b726b708038f270

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+const messages = [];
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+function writeCards(names, event) {
+    for (let n = 0; n < names.length; n++) {
+        messages.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`);
+    }
+return messages;
+}
+writeCards(names);
+*/
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let n = 0; n < names.length; n++) {
+        messages.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`);
+    };
+return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(n){
+    let i = n;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    return n;
+}
+countDown(10);
